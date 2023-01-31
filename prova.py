23627cbd12ecd8fa284085002d4ef40b2f9082d7
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-01-31 15:43:03 +0000

    primo salvataggio

---
+++ prova.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def benvenuto():
+    return ('<h1>BENVENUTO</h1>')
+
+@app.route('/en', methods=['GET'])
+def hello_world():
+    return ('<h1>Hello, world!</h1>')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
