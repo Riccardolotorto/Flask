6ba51eeafe4923c21864403f79ece88a445766e3
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-02-05 16:01:28 +0000

    secondo commit

--- prova.py
+++ prova.py
@@ -9,5 +9,13 @@ def benvenuto():
 def hello_world():
     return ('<h1>Hello, world!</h1>')
 
+@app.route('/contatti', methods=['GET'])
+def contatti():
+    return ('<h1>Contattami! Mi chiamo Riccardo Lotorto</h1>')
+
+@app.route('/inter', methods=['GET'])
+def inter():
+    return ('<h1>Tifo inter</h1>')
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
