f2ab969c50260d36a0867856205667097ad1ec4f
Riccardolotorto <lotorto.riccardo@iisgalvanimi.edu.it>
2023-02-13 12:11:32 +0000

    commit con commenti

--- flask001.py
+++ flask001.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+app = Flask(__name__)   #variabile identificativa del server web
 
 @app.route('/')
+def home():
+  return("<h1>HOMEPAGE</h1>")
+
 @app.route('/en')
 def hello_world():
   return render_template("index.html", Titolo='Welcome', Testo='Hello, world!')
@@ -11,4 +14,6 @@ def ciao_mondo():
   return render_template("index.html", Titolo='Benvenuti', Testo='Ciao, mondo!')
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True)   #codici per eseguire il server web
+
+#indirizzo IP del nostro computer: 0.0.0.0 
